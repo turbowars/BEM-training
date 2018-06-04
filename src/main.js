7bdf2fab0cbce23dfbe7f473bf98d0df6d6cc9d3
Dheeraj Sampath <dheeraj.s@techaspect.com>
2018-06-05 02:12:14 +0530

    boilerplate

---
+++ src/main.js
@@ -0,0 +1,7 @@
+'use strict';
+
+require('./main.scss');
+
+var appContainer = document.querySelector('#app');
+
+//appContainer.innerHTML = '<div class="main-component"><h1 class="main-component__title">Welcome home!!</h1></div>';
